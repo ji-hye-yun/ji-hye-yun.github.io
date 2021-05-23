b9ed1118814bd1f436398f7d047de3f158c39e84
ji-hye-yun <jihyeyunhyeji@gmail.com>
2021-05-23 20:42:23 +0900

    commit

---
+++ asset/js/common.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('html').css('display', 'red');
+
+});
--- asset/js/scroll.js
+++ asset/js/scroll.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
-
-    $(window).on('scroll', function(){
+    $('html').css('display', 'red');
+/*
+    $('#wrap').on('scroll', function(){
         var scroll = $(this).scrollTop();
         
         console.log(scroll)
-    });
-
-
-
+        
+        if ( scroll > 0 ) {
+            $('html').css('display', 'red')
+        }
+    });*/
 });
