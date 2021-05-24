ee843179c5811e22b16fbd3db3cb934e5767b9d6
ji-hye-yun <jihyeyunhyeji@gmail.com>
2021-05-25 01:31:43 +0900

    commit

--- asset/js/common.js
+++ asset/js/common.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     var $skip_a = $('#skip_navi > li > a');
     var $work_li = $('.work_box li');
     var $portfolio_li = $('.portfolio_box li');
+    var $resume = $('.resume_open');
+    var $resume_close = $('.resume_close');
 
     $skip_a.on('focusin', function(){
         $(this).addClass('on');
@@ -11,6 +13,17 @@ $(document).ready(function(){
         $(this).removeClass('on');
     });
 
+    // main .about 
+    $resume.on('click', function(e){
+        e.preventDefault();
+        $('.dim').css('display', 'block');
+        $('#resume').css('display', 'block');
+    });
+    $resume_close.on('click', function(e){
+        e.preventDefault();
+        $('.dim').css('display', 'none');
+        $('#resume').css('display', 'none');
+    });
 
 
     // main .work
@@ -21,6 +34,7 @@ $(document).ready(function(){
         $work_li.removeClass('on');
         $(this).addClass('on');
     });
+
 
     // main .portfolio
     $portfolio_li.on('click', function(){
