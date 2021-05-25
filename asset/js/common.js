cdd373d189501ae54f8454d6e5864531a7333981
ji-hye-yun <jihyeyunhyeji@gmail.com>
2021-05-25 21:08:48 +0900

    commit

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,3 +1,20 @@
+window.onload = function(){
+
+    var $info = document.getElementById('info');
+    var $info_li = document.querySelectorAll('.info_navi > li');
+
+    $info_li.forEach(function(item,index){
+        item.onclick = function(){
+            $info.className = 'info'+(index+1);
+            $info_li.forEach(function(item,index){
+                $info_li[index].className = '';
+            });
+            this.className = 'on';
+        }
+    });
+}
+
+
 $(document).ready(function(){
     var $skip_a = $('#skip_navi > li > a');
     var $work_li = $('.work_box li');
@@ -42,6 +59,31 @@ $(document).ready(function(){
         $portfolio_li.removeClass('on');
         $(this).addClass('on');
     });
+
+
+    // main .contact
+    emailjs.init("user_ZGnOwZr9advSSnnVn6gBe");
+    $('input[name=submit]').click(function(){    
+        
+        alert('성공적으로 메일을 전송하였습니다.')
+        $('input[name=from_name], input[name=phone], input[name=email], textarea[name=message]').val('');
+
+        var templateParams = {	
+            name: $('input[name=from_name]').val(),
+            phone: $('input[name=phone]').val(), 
+            email : $('input[name=email]').val(),
+            message : $('textarea[name=message]').val()
+        }
+            
+        emailjs.send('service_72jmx28', 'template_39qke13', templateParams)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+    });	
+        
+
 
 
 
